d99a64145636e5ca4e8a149203c7b4885216afe2
Tabla2181 <102341979+Tabla2181@users.noreply.github.com>
2022-11-19 19:43:32 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+import ProxyCloud
+
+BOT_TOKEN =  os.environ.get('bot_token','5720354270:AAG2gBtnQVMPyMZJENVcYZGCtoMa3tEJX-M')
+API_ID =  os.environ.get('api_id','15558101')
+API_HASH = os.environ.get('api_hash','c2cbb2f07c44fe466076fbe65e3c00cc')
+SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
+ROOT_PATH = 'root/'
+ACCES_USERS = os.environ.get('Elnietodecacha')
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH'))
+
+if PROXY:
+  print(f'Proxy {PROXY.as_dict_proxy()}')
+
+space = {} #tabla de la base de datos de usario
+space['Elnietodecacha'] = 0 #Tiene q ser valor 0, sino dará error
