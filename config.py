c8051a294b1b2353b019788174bf7242c1b64150
Tabla2181 <102341979+Tabla2181@users.noreply.github.com>
2022-11-24 15:18:02 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+import ProxyCloud
+
+BOT_TOKEN = '5456651222:AAHc3h-fG5CS5jPywnK2aF3LQArXR5Fj8Tw' #Aqui va el token del bot
+API_ID =  15558101 #Tu api id de telegram
+API_HASH = 'c2cbb2f07c44fe466076fbe65e3c00cc' #Tu api id de telegram
+SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
+ROOT_PATH = 'root/'
+ACCES_USERS = os.environ.get('tl_admin_user','Elnietodecacha').split(';')
+
+static_proxy = 'socks5h://KFGEJJYJJDLKFJYGJDLJKGYKCDDGRGHHCGHGCH'
+PROXY = ProxyCloud.parse(static_proxy)
+
+if PROXY:
+  print(f'Proxy {PROXY.as_dict_proxy()}')
+  
+#Lo siguiente son las tablas de la base de datos de usarios, 
+#es obligatorio agregar a aquellos usarios estaticos puestos en el main.py
+#los agregados mediante /add no es necesario
+#ponerlos valor 0 siempre
+
+space = {}
+space['Elnietodecacha'] = 0
+
